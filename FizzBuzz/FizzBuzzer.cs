e9eca032596b5186faaa42915bb88f87dbfd1410
andreyborovkov <andrey_borovkov@yahoo.com>
2018-09-19 11:53:38 -0400

    initial commit

---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Get(int input)
+        {
+            if (isMultipleOf3And5(input))
+            {
+                return "FizzBuzz";
+            }
+            if (isMultipleOf3(input))
+            {
+                return "Fizz";
+            }
+            if (isMultipleOf5(input))
+            {
+                return "Buzz";
+            }
+
+            return input.ToString();
+        }
+
+        private bool isMultipleOf3(int input)
+        {
+            return input % 3 == 0;
+        }
+
+        private bool isMultipleOf5(int input)
+        {
+            return input % 5 == 0;
+        }//reflection
+
+        private bool isMultipleOf3And5(int input)
+        {
+            return isMultipleOf3(input) && isMultipleOf5(input);
+        }
+    }
+}
